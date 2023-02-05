7e973f2b5336b49f8a9fbc394c545865643dc5a8
krasimirkadiyski <94919308+krasimirkadiyski@users.noreply.github.com>
2023-02-05 13:39:48 +0200

    search component

--- 03-frontend/react-library/src/App.tsx
+++ 03-frontend/react-library/src/App.tsx
@@ -4,12 +4,15 @@ import { Carousel } from './layouts/HomePage/components/Carousel';
 import { HomePage } from './layouts/HomePage/HomePage';
 import { Footer } from './layouts/NavbarAndFooter/Footer';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
+import { SearchBook } from './layouts/SearchBooksPage/components/SearchBook';
+import { SearchBooksPage } from './layouts/SearchBooksPage/SearchBooksPage';
 
 export const App = () => {
   return (
     <div>
     <Navbar/>
-    <HomePage/>
+    {/* <HomePage/> */}
+    <SearchBooksPage/>
     <Footer/>
     </div>    
   );
